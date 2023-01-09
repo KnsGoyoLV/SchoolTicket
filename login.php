b8b9c4f7a439ebac4533b154a15bdde24c63286c
DrError <daniss404@gmx.com>
2023-01-09 05:42:35 -0800

    Added microsoft graph login page

--- login.php
+++ login.php
@@ -1,31 +1,12 @@
-<!DOCTYPE html>
-<html lang="lv">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inernet Veikals Ieeja</title>
-    <link rel="stylesheet" href="style_login.css">
-</head>
-<body>
-	<div class="container" id="container">
-		<div class="form-container sign-in-container">
-			<form action="#" method="POST">
-				<img class="logo" src="logo.png">
-						
-				<?php
-
-				
-				
-				
-				?>
-				<h1>Ielogoties sistēmā</h1>
-				<input type="username" placeholder="Lietotājvārds" name="username" />
-				<input type="password" placeholder="Parole" name="userpass"/>
-				<input type= "submit" name="submit1" value="Login"/>
-
-			</form>
-		</div>
-	</div>
-</body>
-</html>
+<?php
+session_start();
+require "vendor/autoload.php";
+use myPHPnotes\Microsoft\Auth;
+$tenant = "091c9fa6-7934-4f1c-b232-ddeea4726d3b";
+$client_id = "a5520baf-956d-48c1-9d59-1697891ecfb1";
+$client_secret = "7908a999-72b2-4e5f-b8fa-7c528c429dae";
+$callback = "http://localhost";
+$scopes = ["https://graph.microsoft.com/.default"];
+$microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
+header("location: " . $microsoft->getAuthUrl());
+?>
