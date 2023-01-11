563a0ce374c63f77df4b820a1004d96edf8e1072
Frishiits <122269330+Frishiits@users.noreply.github.com>
2023-01-11 15:11:29 +0200

    jauna datubaze

--- tb.sql
+++ tb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2023 at 12:49 PM
+-- Generation Time: Jan 11, 2023 at 02:10 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,117 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lietotajs`
+-- Table structure for table `ticket`
 --
 
-CREATE TABLE `lietotajs` (
-  `lietotajs_id` int(11) NOT NULL,
-  `uzvards` varchar(45) NOT NULL,
-  `epasts` varchar(255) NOT NULL,
-  `talrunis` int(8) NOT NULL
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL,
+  `laiks` date NOT NULL,
+  `vards` varchar(255) NOT NULL,
+  `uzvards` varchar(255) NOT NULL,
+  `iela` enum('Vānes iela','Ventspils iela') NOT NULL DEFAULT 'Vānes iela',
+  `klase` varchar(255) NOT NULL,
+  `problema` text NOT NULL,
+  `piezime` text DEFAULT NULL,
+  `apstiprinats` tinyint(1) NOT NULL,
+  `status` enum('Nav iesākts','Iesākts','Pabeigts') DEFAULT 'Nav iesākts'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `piezime`
+-- Dumping data for table `ticket`
 --
 
-CREATE TABLE `piezime` (
-  `piezime_id` int(11) NOT NULL,
-  `piezime` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `status`
---
-
-CREATE TABLE `status` (
-  `status_id` int(11) NOT NULL,
-  `status` enum('Nav iesākts','Iesākts','Pabeigts') NOT NULL DEFAULT 'Nav iesākts',
-  `iesniegsanas_dat` date NOT NULL,
-  `pabeigsanas_dat` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vieta`
---
-
-CREATE TABLE `vieta` (
-  `vieta_id` int(11) NOT NULL,
-  `iela` enum('Ventspils iela','Vānes iela') DEFAULT 'Ventspils iela',
-  `stavs` int(11) NOT NULL,
-  `kabinets` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-CREATE TABLE `apstiprinajums` (
-  `apstiprinajums_id` int(1) NOT NULL,
-  `apstiprinats` boolean
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `ticket` (`ticket_id`, `laiks`, `vards`, `uzvards`, `iela`, `klase`, `problema`, `piezime`, `apstiprinats`, `status`) VALUES
+(1, '2023-01-11', 'Peteris', 'Ozols', 'Ventspils iela', 'A-216', 'Pazuda internets', NULL, 0, 'Nav iesākts'),
+(2, '2023-01-11', 'Reinis', 'Kļava', 'Vānes iela', 'C-205', 'Griesti iejuka', 'Laukā bija vētra un lietus līdz ar to iebruka griesti', 0, 'Nav iesākts');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `lietotajs`
+-- Indexes for table `ticket`
 --
-ALTER TABLE `lietotajs`
-  ADD PRIMARY KEY (`lietotajs_id`);
-
---
--- Indexes for table `piezime`
---
-ALTER TABLE `piezime`
-  ADD PRIMARY KEY (`piezime_id`);
-
---
--- Indexes for table `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`status_id`);
-
-  ALTER TABLE `apstiprinajums`
-  ADD PRIMARY KEY (`apstiprinajums_id`);
-
---
--- Indexes for table `vieta`
---
-ALTER TABLE `vieta`
-  ADD PRIMARY KEY (`vieta_id`);
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `lietotajs`
+-- AUTO_INCREMENT for table `ticket`
 --
-ALTER TABLE `lietotajs`
-  MODIFY `lietotajs_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `piezime`
---
-ALTER TABLE `piezime`
-  MODIFY `piezime_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `status`
---
-ALTER TABLE `status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vieta`
---
-ALTER TABLE `vieta`
-  MODIFY `vieta_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `ticket`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
