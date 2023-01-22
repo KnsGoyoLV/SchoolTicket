3871d51ebd918adc7440eaa5797b563eb2701c4b
DrErrorOwner <daniss404@gmx.com>
2023-01-22 23:40:42 +0200

    some clean up

--- Info.php
+++ Info.php
@@ -8,6 +8,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LVT TicketSupport</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="stylesheet" href="style_main.css">
 
@@ -16,18 +17,15 @@ session_start();
 <body >
 
 <header>
-    <a href="#" class="animate-charcter">Sixsense</a>
+<a href="#" class="animate-charcter">Liepajas Valsts Tehnikums</a>
     <nav class="navbar">
         <a href="index.php"><i class="fas fa-home"></i> Sākumlapa</a>
-        <a href="newinfo.php"><i class="fas fa-circle-info"></i> Informacijas</a>
-        <a href="#"><i class="active" class="fas fa-wifi"></i> Pievienot Rūteri</a>
-        <?php
-        
-        ?>
+        <a href="newinfo.php"> <i class="fas fa-plus"></i>Pievienot atbalsta biļeti</a>
+        <a href="#"><i class="active"  class="fas fa-circle-info"></i>Jaunumi</a>
     </nav>
     <nav class="navbar">
     <a hred="login.php"><b><?php 		
-         echo " <a href='login.php'><b>" . $_SESSION['username'] . "</b> <i class='fas fa-power-off'></i></a>";
+         echo " <a href='login.php'><b>" . $_SESSION['username'] . $_SESSION['surname'] . "</b> <i class='fas fa-power-off'></i></a>";
      ?> </b>
     
     </nav>
@@ -72,7 +70,7 @@ session_start();
 </section>
 
 <footer>
-    Sixsesne, Work fast with ease &copy; 2022
+        Liepajas Valsts Tehnikums &copy; 2023
 </footer>
 
 <script src="files/script.js"></script>
