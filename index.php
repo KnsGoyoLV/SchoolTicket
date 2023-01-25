40d22ab4e26d7f3de344397becef845890f64609
DrErrorWork <daniss404@gmx.com>
2023-01-25 11:50:26 +0200

    New start to design

--- index.php
+++ index.php
@@ -74,35 +74,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LVT TicketSupport</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="stylesheet" href="style_main.css">
 
 </head>
 
 <body>
-
+ 
 <header>
-    <a class="animate-charcter">Liepajas Valsts Tehnikums</a>
-    <nav class="navbar">
-        <a href="#sakums"class="active"><i class="fas fa-home"></i>Sākumlapa</a>
-        <a href="newinfo.php"><i class="fas fa-plus"></i>Pievienot atbalsta biļeti</a>
-        <a href="info.php"><i class="fas fa-circle-info"></i>Informācija</a>
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+      <div class="container">
+        <a href="#" class="navbar-brand">Liepajas Valsts Tehnikums</a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navmenu">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a href="#learn" class="nav-link">Sākum Lapa</a>
+            </li>
+            <li class="nav-item">
+              <a href="#questions" class="nav-link">Pievienot Problēmas</a>
+            </li>
+            <li class="nav-item">
+              <a href="#instructors" class="nav-link">Informācija</a>
+            </li>
+            <li class="nav-item">
+                <a hred="logout.php"><?php 		
+                echo " <a href='logout.php' style='font-family: ui-sans-serif;'>" .$_SESSION['username']." ".$_SESSION['surname']. "<i class='fas fa-power-off'></i></a>";     
+                ?>
+            </nav>
+            </li>    
+          </ul>
+        </div>
+      </div>
     </nav>
-    <nav class="navbar">
-    <a hred="logout.php"><?php 		
-         echo " <a href='logout.php'><b style='font-family: ui-sans-serif;'>" .$_SESSION['username']." ".$_SESSION['surname']. "</b> <i class='fas fa-power-off'></i></a>";
-        
-    ?> 
+
+   
     </nav>
     <div id="menu-btn" class="fas fa-bars"></div>
 </header>
 
-<section id="adminSakums">
-    <div class="row">
-        <div class="info">
-            <div class="head-info"><b>Jūsu atbalsta biļetes:</b></div>
-            <table >
-                <tr>
+
+<br /><br />  
+           <div class="container">  
+                <h3 align="center">TEST123</h3>  
+                <br />  
+                <div class="table-responsive">  
+                     <table id="employee_data" class="table table-striped table-bordered">  
+                     <tr>
                     <th>Datums</th>
                     <th>Iela</th>
                     <th>Klase</th>
@@ -147,14 +176,15 @@
                     echo '</tr>';      
                 }
                 ?>
-            </table>
-        </div>
-    </div>
-    
+                     </table>  
+                </div>  
+           </div>  
+
 </section>
 <footer>
         Liepajas Valsts Tehnikums &copy; 2023
 </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
