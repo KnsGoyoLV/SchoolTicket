c1455419569d777cd00a41c8196352d5c75a754a
DrErrorOwner <daniss404@gmx.com>
2023-01-27 12:55:27 +0200

    Some clean up and comments

--- newinfo.php
+++ newinfo.php
@@ -27,7 +27,6 @@ if(!isset($_SESSION['t'])){
       <div class="container">
         <a href="#" class="navbar-brand">Liepajas Valsts Tehnikums</a>
         
-
         <button
           class="navbar-toggler"
           type="button"
@@ -59,16 +58,14 @@ if(!isset($_SESSION['t'])){
       </div>
     </nav>
 
-   
     </nav>
     <div id="menu-btn" class="fas fa-bars"></div>
 </header>
 <body>
 
-
-
     <form class="container-md" method="post">
      <?php 
+     // if submited then send query to database and add the new row to the table with the new info
         if(isset($_POST['submit1'])){
          $pdo->query("INSERT INTO `pieteikums`  ( `iela`, `telpa`, `status`, `problema`, `piezimes`, `risinajums_risinajums_id`, `epasts`) VALUES
                                                 ('".$_POST['Iela']."', '".$_POST['Telpa']."', 'Neatrisināts', '".$_POST['Prob']."', '".$_POST['Piez']."', '1', '".$_SESSION['email']."')");
@@ -101,6 +98,5 @@ if(!isset($_SESSION['t'])){
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
-
 </body>
 </html>
