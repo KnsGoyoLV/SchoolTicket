a9bda99c3a6fd3e5e4b7554935b7f180b0c235ca
DrErrorWork <daniss404@gmx.com>
2023-01-30 11:42:49 +0200

    new table in work

--- index.php
+++ index.php
@@ -185,116 +185,60 @@
 
 </section>
 
-<table class="table align-middle mb-0 bg-white">
-  <thead class="bg-light">
+<table class="table align-middle mb-2 table-responsive">
+  <thead class="thead-dark">
     <tr>
       <th>Iela un Klase/Datums</th>
       <th>Problema</th>
       <th>Piezime</th>
-      <th>Remonta vai IT darbs</th>
-      <th>Status</th>
+      <th>Statuss</th>
+      <th>IT/Remonta darbs</th>
       <th>Apstipriāt</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
+    <?php
+      $result = $pdo->query("SELECT * FROM pieteikums WHERE epasts ='".$_SESSION['email']."'");
+      $rows = $result->fetchAll();
+    foreach ($rows as $row) {
+      ?>
+         <tr>
       <td>
         <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-              alt=""
-              style="width: 45px; height: 45px"
-              class="rounded-circle"
-              />
-          <div class="ms-3">
-            <p class="fw-bold mb-1">Iela un klase te</p>
-            <p class="text-muted mb-0">Datums</p>
+          <div class="ms-6">
+            <p class="fw-bold mb-1"><?= $row['iela'];?>: <?= $row['telpa'];?></p>
+            <p class="text-muted mb-1"><?= $row['laiks'];?></p>
           </div>
         </div>
       </td>
       <td>
-        <p class="fw-normal mb-1">Software engineer</p>
-        <p class="text-muted mb-0">IT department</p>
+        <p class="fw-normal mb-2"><?= $row['problema'];?></p>
       </td>
       <td>
-        <span class="badge badge-success rounded-pill d-inline">Active</span>
+        <span class="badge badge-success rounded-pill d-inline" style="color: black"><?= $row['piezimes'];?></span>
       </td>
-      <td>Senior</td>
+      <td><?= $row['status'];?></td>
+      <td>TODO:</td>
       <td>
         <button type="button" class="btn btn-link btn-sm btn-rounded">
-          Edit
+          Izdarīts
         </button>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-              class="rounded-circle"
-              alt=""
-              style="width: 45px; height: 45px"
-              />
-          <div class="ms-3">
-            <p class="fw-bold mb-1">Alex Ray</p>
-            <p class="text-muted mb-0">alex.ray@gmail.com</p>
-          </div>
-        </div>
-      </td>
-      <td>
-        <p class="fw-normal mb-1">Consultant</p>
-        <p class="text-muted mb-0">Finance</p>
-      </td>
-      <td>
-        <span class="badge badge-primary rounded-pill d-inline"
-              >Onboarding</span
-          >
-      </td>
-      <td>Junior</td>
-      <td>
-        <button
-                type="button"
-                class="btn btn-link btn-rounded btn-sm fw-bold"
-                data-mdb-ripple-color="dark"
-                >
-          Edit
-        </button>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/7.jpg"
-              class="rounded-circle"
-              alt=""
-              style="width: 45px; height: 45px"
-              />
-          <div class="ms-3">
-            <p class="fw-bold mb-1">Kate Hunington</p>
-            <p class="text-muted mb-0">kate.hunington@gmail.com</p>
-          </div>
-        </div>
-      </td>
-      <td>
-        <p class="fw-normal mb-1">Designer</p>
-        <p class="text-muted mb-0">UI/UX</p>
-      </td>
-      <td>
-        <span class="badge badge-warning rounded-pill d-inline">Awaiting</span>
-      </td>
-      <td>Senior</td>
-      <td>
-        <button
-                type="button"
-                class="btn btn-link btn-rounded btn-sm fw-bold"
-                data-mdb-ripple-color="dark"
-                >
-          Edit
+        <button type="button" class="btn btn-link btn-sm btn-rounded">
+          Neizdarīts
         </button>
       </td>
     </tr>
   </tbody>
+      
+
+
+    <?php
+    }
+
+
+    ?>
+
+   
 </table>    
 
 <div class="footer">
