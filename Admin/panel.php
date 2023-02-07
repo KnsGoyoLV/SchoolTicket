602ca241a8de5bdeb8c8ce98d5ed60ce6785a8c3
DrErrorWork <daniss404@gmx.com>
2023-02-07 11:44:44 +0200

    code optimization

--- Admin/panel.php
+++ Admin/panel.php
@@ -1,56 +1,10 @@
 <?php
-require_once("../database/connectDB.php");
+include("../functions/function.php");
 require "../vendor/autoload.php";
-$env = parse_ini_file('../database/.env');
 session_start();
-
-$tenant =$env['tenant'];
-$client_id = $env['client_id'];
-$client_secret = $env['client_secret'];
-$callback = $env['callback']; 
-
-if (array_key_exists ('access_token', $_POST)){
-    //save access_token to SESSION t variable
-    $_SESSION['t'] = $_POST['access_token'];
-    $t = $_SESSION['t'];
-    $ch = curl_init ();
-    //get users json data from /me/ endpoint
-    curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('Authorization: Bearer '.$t,
-                                           'Conent-type: application/json'));
-    curl_setopt ($ch, CURLOPT_URL, "https://graph.microsoft.com/v1.0/me/");
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    $rez = json_decode (curl_exec ($ch), 1);
-    if (array_key_exists ('error', $rez)){
-        //if we have a error dump everything that error gives us  
-        var_dump ($rez['error']);       
-        die();
-    }
-    else{// get users data from json with $rez variable name
-        $_SESSION['username'] = $rez["givenName"];
-        $_SESSION['surname'] = $rez["surname"];
-        $_SESSION['email'] = $rez['mail'];
-        $_SESSION['id'] = $rez["id"];
-        $_SESSION['job'] = $rez["jobTitle"];
-        $_SESSION['fullname'] = $rez["displayName"];
-    }
-    curl_close ($ch);
-    header ('Location: ' . $callback);
-}
-// if acces token is invalid or not gotten then go back to login.php file
-if(!isset($_SESSION['t'])){
-header('location: login.php');
-}
-
-//Block subdomain 
-$parts = explode('@',  $_SESSION['email']);
-$domain = array_pop($parts);
-$blocked_domains = array('sk');// to block sub domain add sk in here
-
-if ( !$_SESSION['username'] == 'Daniels' && in_array(explode('.', $domain)[0], $blocked_domains)) {
-    header("Location:blocked.php");
-    exit();
-}
-
+MicrosoftInfo();
+Invalid_seasson();
+block_domain();
 ?>
 <!DOCTYPE html>
 <html lang="lv">
@@ -60,18 +14,15 @@ if ( !$_SESSION['username'] == 'Daniels' && in_array(explode('.', $domain)[0], $
     <title>LVT TicketSupport</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-     
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"> 
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!--Main Navigation-->
 <header>
   <!-- Sidebar -->
-  <nav id="sidebarMenu" class="collapse d-xl-block sidebar collapse bg-gray">
+  <nav id="sidebarMenu" class="collapse d-xl-block sidebar collapse " style="background-color: #1c4c7c;">
     <div class="position-sticky">
       <div class="list-group list-group-flush mx-3 mt-4">
         <a
