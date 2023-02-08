f15c90453526debf10db43bb79ea4e8e7a25ded2
DrErrorWork <daniss404@gmx.com>
2023-02-08 10:46:41 +0200

    few changes

--- functions/IsAdmin.php
+++ functions/IsAdmin.php
@@ -1,19 +1,15 @@
 <?php
-    session_start();
+session_start();
     $admin = array(
         'daniels.vidopskis@sk.lvt.lv',
         'arturs.aunins@lvt.lv',
         'gunta.lagzda@lvt.lv',
         'mareks.frismanis@sk.lvt.lv'
     );
-
     if(in_array($_SESSION['email'],$admin)){
         header("location:../Admin/panel.php");
     }else{
         header("location:../blocked.php");
     }
 session_abort();
-
-
-
 ?>
