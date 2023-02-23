a90818c522c7cb0ffeb9fb5e0032134a914947b6
DrErrorWork <daniss404@gmx.com>
2023-02-23 10:15:55 +0200

    some changes

--- Admin/panel.php
+++ Admin/panel.php
@@ -11,7 +11,7 @@ include("..\database\connectDB.php");
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LVT TicketSupport</title>
+  <title>LVT Admin Panel</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
