9904e1275be96ecf4c6d5fa43a642f6ae6b825e7
DrErrorWork <daniss404@gmx.com>
2023-03-01 10:46:26 +0200

    modal

--- pievienot.php
+++ pievienot.php
@@ -106,11 +106,11 @@ if(!isset($_SESSION['t'])){
       <div class="modal-dialog">
           <div class="modal-content">
               <div class="modal-header">
-                  <h5 class="modal-title">TEST123</h5>
+                  <h5 class="modal-title">Kļūda </h5>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                  <p>EU TU IERAKSTI KAUT KO TAGAD '.$empt.'</p>
+                  <p>Lūdzu aizpildiet visas vajadzīgās  '.$empt.'</p>
               </div>
               <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
