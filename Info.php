4fda58b0884224954fc75fda6afedb9a8570856a
DrErrorWork <daniss404@gmx.com>
2023-03-02 14:36:39 +0200

    fixed info php file

--- Info.php
+++ Info.php
@@ -34,14 +34,14 @@ if (!isset($_SESSION['t'])) {
                 <div class="collapse navbar-collapse " id="navmenu">
                     <ul class="navbar-nav ms-auto  ">
                         <li class="nav-item">
-                            <a href="login.php" class="nav-link"><i class="fas fa-home"></i>Sākumlapa</a>
+                            <a href="index.php" class="nav-link"><i class="fas fa-home"></i>Sākumlapa</a>
                         </li>
                         <li class="nav-item">
-                            <a href="pievienot.php" class="nav-link" style="background-color: #4782b5;"><i
+                            <a href="pievienot.php" class="nav-link"><i
                                     class="fas fa-plus"></i>Pievienot Pieteikumu</a>
                         </li>
                         <li class="nav-item">
-                            <a href="info.php" class="nav-link"><i class="fas fa-circle-info"></i>Informācija</a>
+                            <a href="#"style="background-color: #4782b5;" class="nav-link"><i class="fas fa-circle-info"></i>Informācija</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center" href="#"
