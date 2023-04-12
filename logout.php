9c90873dd24e400cc4ca601fa7dd93b5f56db6d6
Dr.Error <56202075+KnsGoyoLV@users.noreply.github.com>
2023-04-12 10:36:38 +0300

    logout

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php 
     require_once("database/connectDB.php");
-    require "vendor/autoload.php";
     $env = parse_ini_file('database/.env');  
     $url = 'https://login.microsoftonline.com/'.$env['tenant'].'/oauth2/logout?post_logout_redirect_uri='.$env['logout'].'';
     header('Location: ' . $url);
