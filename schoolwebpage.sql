7efb7308d4b0a0b125000352227facec4308b944
DrError <daniss404@gmx.com>
2023-06-22 00:29:28 +0300

    new db

--- schoolwebpage.sql
+++ schoolwebpage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2023 at 02:21 PM
+-- Generation Time: Jun 21, 2023 at 11:24 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
